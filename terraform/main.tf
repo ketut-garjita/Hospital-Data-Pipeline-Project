c7dbcaa6decb7333feb5ea0a290dd8d315d9f1b2
Ketut Garjita <77673886+ketut-garjita@users.noreply.github.com>
2025-03-31 15:08:53 +0700

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,14 @@
 provider "google" {
-  project = "de-zoomcamp-2025--id"
-  region  = "asia-southeast2"
+  project = "your project
+  region  = "your region"
 }
 
 resource "google_storage_bucket" "hospital_bucket" {
-  name     = "hospital_datalake2"
-  location = "asia-southeast2"
+  name     = "hospital_datalake"
+  location = "your region"
 }
 
 resource "google_bigquery_dataset" "hospital_dataset" {
-  dataset_id = "hospital2"
+  dataset_id = "hospital"
 }
 
